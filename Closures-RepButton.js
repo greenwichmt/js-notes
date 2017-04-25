1e8b97579fcf7606cdab37ed37fbd9025768a39b
shums <shums@chinaunicom.cn>
2017-04-25 10:16:43 +0800

    closures and js-render-iframe-fatherDOM

---
+++ Closures-RepButton.js
@@ -0,0 +1,60 @@
+﻿http://stackoverflow.com/questions/111102/how-do-javascript-closures-work/
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures
+
+$(document).ready(function () {
+    setLevel1btn();
+});
+var LevelLast = "IfRepeatThenIgnore";
+//T.LEVEL_1,
+//T.LEVEL_2,
+//T.REP_CODE,
+/*设置二级按钮-第一级*/
+function setLevel1btn() {
+    $("#level1btn").empty();
+    for (var i = 0; i < repButton.length; i++) {
+        if (LevelLast == repButton[i].LEVEL_1) continue;
+        LevelLast = repButton[i].LEVEL_1;
+        (function (index) {
+            //console.log(typeof repButton[index].LEVEL_2);
+            if (repButton[index].LEVEL_2 == null || repButton[index].LEVEL_2 == '' || typeof repButton[index].LEVEL_2 == undefined) {
+                $('<a href="#" class="float-left-btn"><svg class="icon" aria-hidden="true">'
+                    + '<use xlink:href="' + repButton[index].LEVEL_1_BTN_CODE + '"></use></svg>'
+                    + '<div>' + repButton[index].LEVEL_1 + '</div></a>').bind('tap', function () {
+                        document.location.href = 'RepDetailFrm.aspx?repCode=' + repButton[index].REP_CODE
+                    }).appendTo('#level1btn');
+            } else {
+                $('<a href="#dialogPage" data-rel="dialog" class="float-left-btn"><svg class="icon" aria-hidden="true">'
+                    + '<use xlink:href="' + repButton[index].LEVEL_1_BTN_CODE + '"></use></svg>'
+                    + '<div>' + repButton[index].LEVEL_1 + '</div></a>').bind('tap', setLevel2btn).appendTo('#level1btn');
+                //console.log(index + repButton[index].LEVEL_1);
+            }
+        })(i)
+    }
+    $("#level1btn").trigger('create');
+}
+String.prototype.trim = function () { return this.replace(/(^\s)|(\s$)/g, '') }
+
+/*设置二级按钮-第二级dialog*/
+function setLevel2btn() {
+    LevelLast = "IfRepeatThenIgnore";
+    var header = this.text.trim();
+    setHeader(header);
+    //console.log(this.text.trim());
+    $('#level2btn').empty();
+    for (var i = 0; i < repButton.length; i++) {
+        //父按钮不同或者已经出现过的二级按钮
+        if (this.text.trim() != repButton[i].LEVEL_1 || LevelLast == repButton[i].LEVEL_2) continue;
+        LevelLast = repButton[i].LEVEL_2;
+        (function (index) {
+            $('<a href="#" data-role="button" data-inline="true" data-mini="true">'
+                + repButton[index].LEVEL_2 + '</a>').on('tap', function () {
+                    document.location.href = 'RepDetailFrm.aspx?repCode=' + repButton[index].REP_CODE
+                }).appendTo('#level2btn');
+            //console.log(repButton[index].LEVEL_2 + '=' + repButton[index].REP_CODE);
+        })(i)
+    }
+    $('#level2btn').trigger('create');
+}
+function setHeader(txt) {
+    $('#dialogPage h1:eq(0)').text(txt);
+}
---
+++ render-iframe-RepWebDetail.js
@@ -0,0 +1,189 @@
+﻿var headerData = [];        //列数组
+var frozenHeaderData = [];  //固定列数组
+
+$(function () {
+    readRepInfo(repCode);
+});
+
+/*通过报表编码查询报表配置信息
+  数据源、备注等*/
+function readRepInfo(repCode) {
+    try {
+        //parent.showLoading('正在读取配置信息……');
+    }
+    catch (err) { }
+    $.ajax({
+        type: "post",
+        url: "../Handlers/RepWebDetailQuery.ashx",
+        dataType: 'json',
+        data: { action: 'repInfo', repCode: repCode },
+        error: function (xmlRequest, textStatus, error) {
+            try {
+                parent.hideLoading();
+            }
+            catch (err) { }
+            alert('读取报表信息异常 - ' + textStatus);
+        },
+        success: function (data) {
+            $('#divMemo').html(data.memo);  //设置备注
+            $('#hTitle').html('&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;' + data.title);  //设置报表标题
+            $('#txtTimest').textbox('setValue', data.timest);   //设置报表时间
+            try {
+                fixColsCount = parseInt(data.fixColsCount); //固定列数
+            }
+            catch (err) { fixColsCount = 0; }
+            try {
+                parent.hideLoading();
+            }
+            catch (err) { }
+            readColsInfo(repCode);  //读取报表列信息
+        }
+    });
+}
+
+/*读取报表列配置信息*/
+function readColsInfo(repCode) {
+    try {
+        //parent.showLoading('正在读取字段信息……');
+    }
+    catch (err) { }
+    $.ajax({
+        type: "post",
+        url: "../Handlers/RepWebDetailQuery.ashx",
+        data: { action: 'cols', repCode: repCode },
+        error: function (xmlRequest, textStatus, error) {
+            try {
+                parent.hideLoading();
+            }
+            catch (err) { }
+            alert('读取列配置信息异常 - ' + textStatus);
+        },
+        success: function (data) {
+            data = data.replace(/[\r\n]/gi, '');    //替换换行符，避免转换JSON出错
+            var fieldData = eval('(' + data + ')'); //把数据转换为json
+            var rowIdx;
+            $.each(fieldData, function (idx, val) {
+                //第一行 或 新开一行，新开数组
+                if (rowIdx == null || rowIdx == undefined || rowIdx != parseInt(val.ROW_IDX)) {
+                    headerData.push([]);
+                    rowIdx = parseInt(val.ROW_IDX);
+                }
+                //datagrid列设置对象
+                var colData = {
+                    title: '<b>' + val.COL_NAME + '</b>',
+                    width: (val.COL_WIDTH == '' || val.COL_WIDTH == '0') ? 80 : parseInt(val.COL_WIDTH),
+                    styler: function (value, row, index) {
+                        if (value == undefined || value == null)
+                            return '';
+                        //如果是百分号，那么>0显示红色，<0显示绿色
+                        if (!isNaN(value.replace('%', ''))) {
+                            var css = 'text-align:right; font-size:.8em; font-weight:bold;';
+                            if (value.indexOf('%') != -1) {
+                                if (parseFloat(value.replace('%', '')) > 0)
+                                    css += 'color:Red;';
+                                else if (parseFloat(value.replace('%', '')) < 0)
+                                    css += 'color:Green;';
+                            }
+                            return css;
+                        }
+                    },
+                    formatter: function (value, row, index) {
+                        if (value == undefined || value == null) {
+                            return '字段异常';
+                        }
+                        else
+                            return value;
+                    }
+                };
+                if (val.FIELD_NAME != '')
+                    colData.field = val.FIELD_NAME.toUpperCase();
+                if (rowIdx == 1 && fixColsCount != undefined &&
+                    fixColsCount != null && fixColsCount > 0 &&
+                    idx < fixColsCount) {
+                    if (frozenHeaderData.length == 0)
+                        frozenHeaderData.push([]);
+                    frozenHeaderData[0].push(colData);
+                }
+                else {
+                    if (val.COL_SPAN != '')
+                        colData.colspan = parseInt(val.COL_SPAN);
+                    if (val.ROW_SPAN != '')
+                        colData.rowspan = parseInt(val.ROW_SPAN);
+                    headerData[rowIdx - 1].push(colData);
+                }
+            });
+            try {
+                parent.hideLoading();
+            }
+            catch (err) { }
+            readRepData(repCode);
+        }
+    });
+}
+
+/*读取报表数据源*/
+function readRepData(repCode) {
+    try {
+        parent.showLoading('正在加载数据……');
+    }
+    catch (err) { }
+    if (repCode == undefined || repCode == null)
+        repCode = $('#hiddentRepCode').val();
+    var timest = $('#txtTimest').textbox('getValue').replace(/-/g, '');
+    $.ajax({
+        type: "post",
+        url: "../Handlers/RepWebDetailQuery.ashx",
+        dataType: 'json',
+        data: { action: 'getDatasource', repCode: repCode, repName: $('#hTitle').html(), timest: timest },
+        error: function (xmlRequest, textStatus, error) {
+            try {
+                parent.hideLoading();
+            }
+            catch (err) { }
+            alert('读取数据源异常 - ' + textStatus);
+        },
+        success: function (dataSource) {
+            $('#divMemo').html(dataSource.memo);
+            var memoDiv = null;
+            if ($('#divMemo').html() != '')
+                memoDiv = $('#divMemo');
+            $('#dg').datagrid({
+                frozenColumns: frozenHeaderData,
+                columns: headerData,
+                fit: true,
+                data: dataSource.data,
+                footer: memoDiv
+            });
+            try {
+                parent.hideLoading();
+                readTitleBtnInfo(repCode);
+            }
+            catch (err) { }
+        }
+    });
+}
+
+//读取相似报表按钮
+function readTitleBtnInfo(repCode) {
+    $.ajax({
+        type: "post",
+        url: "../Handlers/RepWebDetailQuery.ashx",
+        dataType: 'json',
+        data: { action: 'similarBtn', repCode: repCode },
+        error: function (xmlRequest, textstatus, error) {
+            alert('读取相似报表按钮出错 - ' + textstatus);
+        },
+        success: function (data) {
+            //$("#headContainer >div").remove();
+            $.each(data, function (idx, val) {
+                //$adiv = $('<div class="m-right m-float-right"><a href="RepWebDetail.aspx?repCode=' + val.REP_CODE + '" target="_self" class="easyui-linkbutton m-next" plain="true" outline="true">' + val.SIMILAR_ALIAS + '</a></div>').insertAfter('#hTitle');;
+                $adiv = $('<div class="m-right m-float-right"><a href="#" class="easyui-linkbutton m-next" plain="true" outline="true">' + val.SIMILAR_ALIAS + '</a></div>');
+                $adiv.bind('click', function (event) {
+                    $('#frmRpt', window.parent.document).attr('src', 'RepWebDetail.aspx?repCode=' + val.REP_CODE);
+                    event.preventDefault();
+                }).insertAfter('#hTitle');
+            })
+            $.parser.parse($('#headContainer'));
+        }
+    })
+}
